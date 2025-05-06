90a53c9f654e57d885ac27182f235e22b7851ea7
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-06 19:48:07 +0700

    Add User entities with Single Table Inheritance (Admin, EndUser, PropertyOwner)

---
+++ src/main/java/id/kostfinder/app/user/model/EndUser.java
@@ -0,0 +1,22 @@
+package id.kostfinder.app.user.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+import java.time.LocalDate;
+
+@Entity
+@DiscriminatorValue("END_USER")
+public class EndUser extends User {
+    private String username;
+    @Column(name = "profile_picture")
+    private String profilePicture;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    private String gender;
+    private String occupation;
+}
---
+++ src/main/java/id/kostfinder/app/user/model/PropertyOwner.java
@@ -0,0 +1,19 @@
+package id.kostfinder.app.user.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("PROPERTY_OWNER")
+public class PropertyOwner extends User {
+    @Column(name = "business_name")
+    private String businessName;
+    private String address;
+    @Column(name = "is_verified")
+    private Boolean isVerified;
+    @Column(name = "bank_name")
+    private String bankName;
+    @Column(name = "bank_account_number")
+    private String bankAccountNumber;
+}
