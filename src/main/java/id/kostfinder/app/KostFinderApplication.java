ae33a8b100aa3a0a28ec8f0df663f23e9346f932
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-06 10:32:25 +0700

    Initialize KostFinder Apps

---
+++ src/main/java/id/kostfinder/app/KostFinderApplication.java
@@ -0,0 +1,13 @@
+package id.kostfinder.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KostFinderApplication.class, args);
+	}
+
+}
