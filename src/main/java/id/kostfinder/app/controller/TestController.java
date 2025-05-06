8d53e56906458fc6d2e99d8906978eafa27a3c9d
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-06 11:25:10 +0700

    Testing controller and auto reload

---
+++ src/main/java/id/kostfinder/app/controller/TestController.java
@@ -0,0 +1,15 @@
+package id.kostfinder.app.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/ping")
+    ResponseEntity<String> ping() {
+        return ResponseEntity.ok("All is well");
+    }
+}
