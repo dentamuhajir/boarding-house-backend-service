6ed67270d46bde31848126ae2b92f21e3f6cb25e
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-08 17:54:18 +0700

    Save entity to repository from services

--- src/main/java/id/kostfinder/app/test/TestController.java
+++ src/main/java/id/kostfinder/app/test/TestController.java
@@ -1,6 +1,8 @@
 package id.kostfinder.app.test;
 
 
+import id.kostfinder.app.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/ping")
     ResponseEntity<String> ping() {
         return ResponseEntity.ok("All is well");
+    }
+    @GetMapping("/test-user-service")
+    ResponseEntity<?> testUserService() {
+        String result = userService.testUserService();
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/id/kostfinder/app/user/service/impl/UserServiceImpl.java
+++ src/main/java/id/kostfinder/app/user/service/impl/UserServiceImpl.java
@@ -7,16 +7,19 @@ import id.kostfinder.app.user.repository.EndUserRepository;
 import id.kostfinder.app.user.repository.PropertyOwnerRepository;
 import id.kostfinder.app.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
-    EndUserRepository endUserRepository;
+    private EndUserRepository endUserRepository;
 
     @Override
-    public void testUserService() {
+    public String testUserService() {
         EndUser endUser1 = new EndUser();
-        //endUser1.setName("Jane Doe");
+        endUser1.setName("Jane Doe");
         endUser1.setEmail("jane.doe@example.com");
         endUser1.setPassword("securePassword123");
         endUser1.setUsername("janedoe");
@@ -25,5 +28,11 @@ public class UserServiceImpl implements UserService {
         endUser1.setDateOfBirth(LocalDate.of(1995, 4, 15));
         endUser1.setGender("Female");
         endUser1.setOccupation("Software Engineer");
+        endUser1 = endUserRepository.save(endUser1);
+        if(endUser1 != null){
+            return "Data save successfully";
+        } else {
+            return "Data cannot save";
+        }
     }
 }
