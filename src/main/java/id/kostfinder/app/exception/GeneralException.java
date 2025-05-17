583141ec6e35a17e86ca68737243530b140943df
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-17 14:44:03 +0700

    Create Exception and implement it on try catch block and simulate to throw the exception

---
+++ src/main/java/id/kostfinder/app/exception/GeneralException.java
@@ -0,0 +1,22 @@
+package id.kostfinder.app.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+public class GeneralException extends RuntimeException{
+    private Integer code;
+    private HttpStatus httpStatus;
+    public GeneralException(Integer code,String message) {
+        super(message);
+        this.code = code;
+        httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public HttpStatusCode getHttpStatus() {
+        return httpStatus;
+    }
+}
