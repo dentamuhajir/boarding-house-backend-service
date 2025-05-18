3233376d8256daaf69085862dfd03d726e265d04
Denta Muhajir <dentamuhajir@gmail.com>
2025-05-18 13:28:02 +0700

    Add Dto ro response List User but Abstract user can't get property from end user

---
+++ src/main/java/id/kostfinder/app/user/dto/response/UserListResponseDTO.java
@@ -0,0 +1,14 @@
+package id.kostfinder.app.user.dto.response;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+public class UserListResponseDTO {
+    private String name;
+    private String email;
+    private String profilePicture;
+    private String phoneNumber;
+}
