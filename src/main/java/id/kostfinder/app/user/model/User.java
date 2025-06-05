d6e4e625a6c70cf9aecfd4fe97cf379ac7de733b
Denta Muhajir <dentamuhajir@gmail.com>
2025-06-06 01:47:17 +0700

    Add softDelete using default query function

--- src/main/java/id/kostfinder/app/user/model/User.java
+++ src/main/java/id/kostfinder/app/user/model/User.java
@@ -2,11 +2,14 @@ package id.kostfinder.app.user.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@Table(name = "user")
 @Data
+@SQLDelete(sql = "Update users SET deleted = true WHERE id = ?")
+@Where(clause = "deleted=false")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
 public abstract class User {
@@ -16,4 +19,5 @@ public abstract class User {
     private String name;
     private String email;
     private String password;
+    private Boolean deleted = Boolean.FALSE;
 }
