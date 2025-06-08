3923dfd901bba32d549eff0dc797574e00d69ad1
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-06-09 03:02:56 +0700

    Setup Spring Security

---
+++ src/main/java/id/kostfinder/app/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package id.kostfinder.app.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
