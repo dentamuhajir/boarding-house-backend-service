c31f1f7983ff6dafd58c98907a06a61b81ac048c
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-06-09 03:32:21 +0700

    Setup Securoty Filter Chain

--- src/main/java/id/kostfinder/app/security/SecurityConfig.java
+++ src/main/java/id/kostfinder/app/security/SecurityConfig.java
@@ -2,10 +2,24 @@ package id.kostfinder.app.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // In REST we are no need csrf
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/register", "/api/login").permitAll() // by default if we activated spring security that means all enpoint is protected so w permitted login and register
+                        .anyRequest().authenticated()
+                )
+                .formLogin().disable(); // We use REST not FORM so we disabled it
+
+        return http.build();
+    }
 
 }
