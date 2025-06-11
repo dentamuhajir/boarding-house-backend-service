2571afc6bb2bf8667af3676c7947433bd2074d4d
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-06-12 00:51:48 +0700

    Change location of passwordEncoder to AppCOnfig to avoid Curcular dependency and implemnt it in seeder

--- src/main/java/id/kostfinder/app/security/SecurityConfig.java
+++ src/main/java/id/kostfinder/app/security/SecurityConfig.java
@@ -46,10 +46,6 @@ public class SecurityConfig {
                     .build();
         };
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    
 
 }
