dfc44efedcbadb9f620ad245b8d85a36e1f2d694
Denta Muhajir <denta.muhajir@binus.ac.id>
2025-06-14 19:08:52 +0700

    Add LoginRequestDTO and implement it in auth Controller

--- src/main/java/id/kostfinder/app/security/controller/AuthController.java
+++ src/main/java/id/kostfinder/app/security/controller/AuthController.java
@@ -1,17 +1,16 @@
 package id.kostfinder.app.security.controller;
 
+import id.kostfinder.app.security.dto.request.LoginRequestDTO;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
 
     @PostMapping("/login")
-    public ResponseEntity<?> login() {
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDTO dto) {
         return ResponseEntity.ok("Success");
     }
 }
